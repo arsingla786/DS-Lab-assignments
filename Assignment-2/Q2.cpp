3003a613c438f643f5948b9934f57d2469413db1
arnav <arnavsingla06@gmail.com>
2025-08-11 10:28:02 +0530

    ass2

---
+++ Assignment-2/Q2.cpp
@@ -0,0 +1,24 @@
+//BUBBLE SORT
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    cout<<"enter array ";
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    cout<<"\nsorted array = ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
