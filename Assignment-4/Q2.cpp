098ca09dfef2b0c9b35227f2f9b2ac40d3169800
Arnav Singla <arnavsingla06@gmail.com>
2025-09-03 15:03:19 +0530

    Update Q2.cpp

--- Assignment-4/Q2.cpp
+++ Assignment-4/Q2.cpp
@@ -1,1 +1,26 @@
+
+//first non repeating elements
+
+#include<iostream>
+#include<string>
+#include<queue>
+using namespace std;
+
+int main(){
+    string str = "aabbcdde";
+    int arr[26] = {0};
+    queue<char> q;
+    for(int i=0;i<str.length();i++){
+        arr[str[i]-'a']++;
+        q.push(str[i]);
+    }
+    
+    for(int i = 0;i<str.length();i++){
+        if(arr[str[i]-'a']==1){
+            cout<<"first non repeating = "<< str[i];
+            break;
+        }
+    }
+    
+}
  
