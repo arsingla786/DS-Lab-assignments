48af89bf0b355ba83d8893ca34cb0fc3be712138
arnav <arnavsingla06@gmail.com>
2025-09-03 10:22:07 +0530

    add

--- Assignment-3/Additional.cpp
+++ Assignment-3/Additional.cpp
@@ -66,3 +66,86 @@ int main() {
 
     return 0;
 }
+
+
+
+/*q4 */
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> dailyTemperatures(vector<int>& temperatures) {
+    int n = temperatures.size();
+    vector<int> answer(n, 0);
+    stack<int> st; // stores indices of days
+    
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+            int prevDay = st.top();
+            st.pop();
+            answer[prevDay] = i - prevDay; // days waited
+        }
+        st.push(i); // push current day
+    }
+    
+    return answer;
+}
+
+int main() {
+    vector<int> temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+    vector<int> result = dailyTemperatures(temperatures);
+    
+    cout << "Result: ";
+    for (int days : result) {
+        cout << days << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
+
+/*q5 */
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canTransform(vector<int>& A) {
+    int n = A.size();
+    vector<int> sortedA = A;
+    sort(sortedA.begin(), sortedA.end());  // target order
+    
+    stack<int> S;
+    vector<int> B;
+    int i = 0; // pointer in A
+    int j = 0; // pointer in sortedA (next needed element)
+    
+    while (i < n || !S.empty()) {
+        // if stack top matches needed element, pop to B
+        if (!S.empty() && S.top() == sortedA[j]) {
+            B.push_back(S.top());
+            S.pop();
+            j++;
+        }
+        // else if we still have elements in A, push into stack
+        else if (i < n) {
+            S.push(A[i]);
+            i++;
+        }
+        // else we are stuck (cannot match needed element)
+        else {
+            return false;
+        }
+    }
+    
+    // if we successfully matched all
+    return (B == sortedA);
+}
+
+int main() {
+    vector<int> A = {3, 1, 2};  // Example input
+    if (canTransform(A)) {
+        cout << "Yes, it is possible." << endl;
+    } else {
+        cout << "No, it is not possible." << endl;
+    }
+    return 0;
+}
+   
