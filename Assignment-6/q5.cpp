bf3a723241564d24b1028584eb42ef7207f869e6
Arnav Singla <arnavsingla06@gmail.com>
2025-11-06 00:51:31 +0530

    Update q5.cpp

--- Assignment-6/q5.cpp
+++ Assignment-6/q5.cpp
@@ -1,2 +1,37 @@
-//Write a program to check if a linked list is Circular Linked List or not
 
+//check if a linked list is cuircular or not 
+
+#include <iostream>
+using namespace std;
+
+class node{
+  public:
+  int data;
+  node* next;
+  public:
+  node(int data){
+    this->data=data;
+    next = NULL;
+  }
+};
+
+bool isCircular(node* head){
+  if (head==NULL) return true;
+  node* temp = head->next;
+  while(temp!=NULL && temp!=head){
+    temp =temp->next;    
+  }
+  return temp==head;
+}
+
+int main() 
+{
+    node* head = new node(1);
+    head->next = new node(2);
+    head->next->next = new node(3);
+    head->next->next = head;
+    
+    cout<<isCircular(head);
+    
+    
+}
